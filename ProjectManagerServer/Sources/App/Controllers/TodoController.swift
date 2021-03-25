e53f663d69533b6610a31cd8387dafcbb69ce6b0
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-03-25 20:51:56 +0900

    [#9] Feat: CRUD 메서드를 TodoController로 이동

--- ProjectManagerServer/Sources/App/Controllers/TodoController.swift
+++ ProjectManagerServer/Sources/App/Controllers/TodoController.swift
@@ -8,34 +8,50 @@
 import Fluent
 import Vapor
 
-//struct TodoController: RouteCollection {
-//    func boot(routes: RoutesBuilder) throws {
-//        let todos = routes.grouped("todos")
-//        todos.get(use: readAll)
-//        todos.get(":todoID", use: read)
-//        todos.post(use: create)
-//        todos.delete(":todoID", use: delete)
-//    }
-//
-//    func readAll(req: Request) throws -> EventLoopFuture<[Todo]> {
-//        return Todo.query(on: req.db).all()
-//    }
-//    
-//    func read(req: Request) throws -> EventLoopFuture<Todo> {
-//        return Todo.find(req.parameters.get("todoID"), on: req.db)
-//            .unwrap(or: Abort(.notFound))
-//    }
-//
-//    func create(req: Request) throws -> EventLoopFuture<Todo> {
-//        let todo = try req.content.decode(Todo.self)
-//        return todo.save(on: req.db).map { todo }
-//    }
-//
-//    func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
-//        return Todo.find(req.parameters.get("todoID"), on: req.db)
-//            .unwrap(or: Abort(.notFound))
-//            .flatMap { $0.delete(on: req.db) }
-//            .transform(to: .ok)
-//    }
-//}
+struct TodoController: RouteCollection {
+    func boot(routes: RoutesBuilder) throws {
+        routes.post("todo", use: createHandler)
+        routes.get("todos", use: getAllHandler)
+        routes.get("todos", ":todoID", use: getHandler)
+        routes.patch("todos", ":todoID", use: updateHandler)
+        routes.delete("todos", ":todoID", use: deleteHandler)
+    }
+    
+    func createHandler(_ req: Request) throws -> EventLoopFuture<Todo> {
+        let todo = try req.content.decode(Todo.self)
+        return todo.save(on: req.db).map { todo }
+    }
+    
+    func getAllHandler(_ req: Request) throws -> EventLoopFuture<[Todo]> {
+        Todo.query(on: req.db).all()
+    }
+    
+    func getHandler(_ req: Request) throws -> EventLoopFuture<Todo> {
+        Todo.find(req.parameters.get("todoID"), on: req.db).unwrap(or: Abort(.notFound))
+    }
+    
+    func updateHandler(_ req: Request) throws -> EventLoopFuture<Todo> {
+        let updatedTodo = try req.content.decode(Todo.self)
+        return Todo.find(req.parameters.get("todoID"), on: req.db)
+            .unwrap(or: Abort(.notFound)).flatMap { todo in
+                todo.title = updatedTodo.title
+                todo.description = updatedTodo.description
+                todo.deadline = updatedTodo.deadline
+                todo.status = updatedTodo.status
+                
+                return todo.save(on: req.db).map {
+                    todo
+                }
+            }
+    }
+    
+    func deleteHandler(_ req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        Todo.find(req.parameters.get("todoID"), on: req.db)
+            .unwrap(or: Abort(.notFound))
+            .flatMap { todo in
+                todo.delete(on: req.db)
+                    .transform(to: .noContent)
+            }
+    }
+}
 
